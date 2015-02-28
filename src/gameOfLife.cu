38110312cb606b4ef1c020f044dceae9f0bfb0a1
Alfonso Ros <alfonso@samantha>
2015-02-28 14:28:31 +0100

    Code simplification and comments

--- src/gameOfLife.cu
+++ src/gameOfLife.cu
@@ -1,10 +1,10 @@
 #include <cuda_runtime.h>
-#include "gameOfLife.h"
+#include <gameOfLife.h>
 
 #define BLOCK_SIZE_X 16
 #define BLOCK_SIZE_Y 16
 
-__global__ void gameOfLifeIt(unsigned char* d_dst, unsigned char* d_buff, const size_t width, const size_t height) {
+__global__ void gameOfLifeKernel(unsigned char* d_src, unsigned char* d_dst, const size_t width, const size_t height) {
     extern __shared__ unsigned char board_sh[];
 
     size_t glob_x = blockDim.x * blockIdx.x + threadIdx.x;
@@ -19,7 +19,7 @@ __global__ void gameOfLifeIt(unsigned char* d_dst, unsigned char* d_buff, const 
 
     bool isActive = (glob_x < width && glob_y < height);
     
-    // Copy board to shared memory
+    // Set shared memory
     for (int share_idx = index; share_idx < share_size; share_idx += (blockDim.x * blockDim.y)) {
 
         int x_img = (blockDim.x * blockIdx.x - 1) + (share_idx % share_width);
@@ -37,7 +37,7 @@ __global__ void gameOfLifeIt(unsigned char* d_dst, unsigned char* d_buff, const 
             y_img = 0;
         }
 
-        board_sh[share_idx] = d_dst[width * y_img + x_img];
+        board_sh[share_idx] = d_src[width * y_img + x_img];
     }
 
     __syncthreads();
@@ -46,39 +46,40 @@ __global__ void gameOfLifeIt(unsigned char* d_dst, unsigned char* d_buff, const 
 
         unsigned char me = board_sh[share_width * (threadIdx.y + 1) + threadIdx.x + 1];
    
-        int count = 0;
-        for (int i = -1; i < 2; i++) {
-            for (int j = -1; j < 2; j++) {
-                count += board_sh[share_width * (threadIdx.y + 1 + j) + threadIdx.x + 1 + i];
-            }
-        } 
-        count -= me;
+        // Count neighbors
+        int count = board_sh[share_width * (threadIdx.y) + threadIdx.x];
+        count += board_sh[share_width * (threadIdx.y) + threadIdx.x + 1];
+        count += board_sh[share_width * (threadIdx.y) + threadIdx.x + 2];
+        count += board_sh[share_width * (threadIdx.y + 1) + threadIdx.x];
+        count += board_sh[share_width * (threadIdx.y + 1) + threadIdx.x + 2];
+        count += board_sh[share_width * (threadIdx.y + 2) + threadIdx.x];
+        count += board_sh[share_width * (threadIdx.y + 2) + threadIdx.x + 1];
+        count += board_sh[share_width * (threadIdx.y + 2) + threadIdx.x + 2];
 
         // Game of life rules
         if (me == 1) {
             if (count < 2) {
-                d_buff[glob_idx] = 0;
+                d_dst[glob_idx] = 0;
             }
             else if (count < 4) {
-                d_buff[glob_idx] = 1;
+                d_dst[glob_idx] = 1;
             }
             else {
-                d_buff[glob_idx] = 0;
+                d_dst[glob_idx] = 0;
             }
         } else {
             if (count == 3) {
-                d_buff[glob_idx] = 1;
+                d_dst[glob_idx] = 1;
             }
         }
     }
 }
 
-void runGameOfLife(unsigned char* d_dst, unsigned char* d_buff, const size_t width, const size_t height) {
+void runGameOfLifeIteration(unsigned char* d_src, unsigned char* d_dst, const size_t width, const size_t height) {
     dim3 threads(BLOCK_SIZE_X, BLOCK_SIZE_Y);
     dim3 grid((width + BLOCK_SIZE_X - 1) / BLOCK_SIZE_X, (height + BLOCK_SIZE_Y - 1) / BLOCK_SIZE_Y);
 
     size_t shared_size = (threads.x + 2) * (threads.y + 2);
 
-    gameOfLifeIt<<<threads, grid, shared_size>>>(d_dst, d_buff, width, height);
-    cudaMemcpy(d_dst, d_buff, width * height * sizeof(unsigned char), cudaMemcpyDeviceToDevice);
+    gameOfLifeKernel<<<threads, grid, shared_size>>>(d_src, d_dst, width, height);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
 #include <time.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <cuda_runtime.h>
 
-#include "gameOfLife.h"
+#include <gameOfLife.h>
+#include <BoardVisualization.hpp>
 
-#define BOARD_SIZE_X 100
-#define BOARD_SIZE_Y 100
-#define CIRCLE_RADIUS 5
+#define BOARD_SIZE_X 200
+#define BOARD_SIZE_Y 200
+#define CIRCLE_RADIUS 2
 
-#define cudaCheckError(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+#define cudaCheckError(ans) { cudaCheckErrors((ans), __FILE__, __LINE__); }
+inline void cudaCheckErrors(cudaError_t code, const char *file, int line, bool abort=true)
 {
    if (code != cudaSuccess) 
    {
@@ -20,54 +20,48 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-
-void displayBoard(const cv::Mat& board, cv::Mat& image, const int radius) {
-    for (int i = 0, x = radius; i < BOARD_SIZE_X; i++, x += 2 * radius) {
-        for (int j = 0, y = radius; j < BOARD_SIZE_Y; j++, y += 2 * radius) {
-            cv::Point2i p(x, y);
-            if (board.at<unsigned char>(i, j) == 1) {
-                cv::circle(image, p, radius, 255, CV_FILLED, 8, 0);
-            } else {
-                cv::circle(image, p, radius, 0, CV_FILLED, 8, 0);
-            }
+void initBoardAtRandom(cv::Mat& board) {
+    srand(time(NULL));
+    for (int i = 0; i < board.cols; i++) {
+        for (int j = 0; j < board.rows; j++) {
+            board.at<unsigned char>(i, j) = static_cast<unsigned char>(rand() % 2);
         }
     }
 }
 
+
 int main(int argc, const char *argv[])
 {
+    unsigned char* d_src;
     unsigned char* d_dst;
-    unsigned char* d_buff;
+
     cv::Mat board = cv::Mat::zeros(BOARD_SIZE_X, BOARD_SIZE_Y, CV_8UC1);
-    cv::Mat image = cv::Mat::zeros(2 * CIRCLE_RADIUS * BOARD_SIZE_X, 2 * CIRCLE_RADIUS * BOARD_SIZE_Y, CV_8UC1);
+    BoardVisualization viewer(BOARD_SIZE_X, BOARD_SIZE_Y, CIRCLE_RADIUS);
 
     // Initialize the board randomly
-    srand(time(NULL));
-    for (int i = 0; i < BOARD_SIZE_X; i++) {
-        for (int j = 0; j < BOARD_SIZE_Y; j++) {
-            board.at<unsigned char>(i, j) = rand() % 2;
-        }
-    }
+    initBoardAtRandom(board);
 
-    // Initialize device board
+    // Initialize device memory
+    cudaCheckError(cudaMalloc((void**)&d_src, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char)));
     cudaCheckError(cudaMalloc((void**)&d_dst, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char)));
-    cudaCheckError(cudaMalloc((void**)&d_buff, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char)));
     cudaCheckError(cudaMemcpy(d_dst, board.data, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char), cudaMemcpyHostToDevice));
 
-    cv::namedWindow("Game of Life", CV_WINDOW_AUTOSIZE);
-
     int key;
-    while (key = cv::waitKey(30)) {
-        runGameOfLife(d_dst, d_buff, BOARD_SIZE_X, BOARD_SIZE_Y);
-        cudaCheckError(cudaMemcpy(board.data, d_dst, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char), cudaMemcpyDeviceToHost));
-        displayBoard(board, image, CIRCLE_RADIUS);
-        cv::imshow("Game of Life", image);
+    while (key = cv::waitKey(10)) {
 
+        // Calling the kernel
+        runGameOfLifeIteration(d_src, d_dst, BOARD_SIZE_X, BOARD_SIZE_Y);
+        cudaCheckError(cudaMemcpy(board.data, d_dst, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char), cudaMemcpyDeviceToHost));
+        cudaCheckError(cudaMemcpy(d_src, d_dst, BOARD_SIZE_X * BOARD_SIZE_Y * sizeof(unsigned char), cudaMemcpyDeviceToDevice));
+
+        /** This is just for display. You should not touch this.  **/
+        viewer.displayBoard(board);
         if (key != -1) break;
     }
-
+ 
+    // Free device memory
+    cudaCheckError(cudaFree(d_src));
     cudaCheckError(cudaFree(d_dst));
-    cudaCheckError(cudaFree(d_buff));
 
     return 0;
 }
